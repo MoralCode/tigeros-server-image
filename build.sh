2ebed34d18519645f07ae75cd2573bfb1c7bc046
Adrian Edwards <adrian@adriancedwards.com>
2024-08-08 22:29:37 -0400

    build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+sudo podman run \
+    --rm \
+    -it \
+    --privileged \
+    --pull=newer \
+    --security-opt label=type:unconfined_t \
+    -v $(pwd)/config.toml:/config.toml:ro \
+    -v $(pwd)/output:/output \
+    -v /var/lib/containers/storage:/var/lib/containers/storage \
+    quay.io/centos-bootc/bootc-image-builder:latest \
+    --type qcow2 \
+    --local \
+    quay.io/fedora/fedora-bootc:40
